9358314ca4f92add349a4836b76a334c77c61874
Agustín Jiménez Fernández <agustinjf87@gmail.com>
2019-03-11 19:10:11 +0000

    form assignment

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('signupForm') signupForm: NgForm;
+
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+
+
+  onSubmit() {
+    console.log(this.signupForm).value;
+  }
+
+}
